54cbfc227604779c5cd23ab89558c4f97707efd6
Zhicheng Wang <zcwang@thoughtworks.com>
2018-03-01 19:22:32 +0800

    fix: auto rerun jasmine when add or remove file

--- gulpfile.ts
+++ gulpfile.ts
@@ -2,7 +2,7 @@ import {src, start, task, watch} from 'gulp';
 import * as jasmine from 'gulp-jasmine';
 
 task('jasmine', () => {
-  return src('./src/app/**/*.ut.ts')
+  return src('src/app/**/*.ut.ts')
     .pipe(jasmine({
       errorOnFail: false,
     }));
@@ -10,5 +10,5 @@ task('jasmine', () => {
 
 task('default', () => {
   start('jasmine');
-  return watch('./src/app/**/*.ts', {cwd: './'}, ['jasmine']);
+  return watch('src/app/**/*.ts', {cwd: './'}, ['jasmine']);
 });
